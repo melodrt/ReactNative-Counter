0d5271adf42616597372452653b8119a6e27d010
meloduarte <devscustomers@gmail.com>
2025-11-12 11:20:25 -0500

    create counter app ans styles

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button } from 'react-native/types_generated/index';
 
 export default function App() {
+  const [count, setCount] = useState(10);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.texHuge}>{count}</Text>
+      <Pressable
+      style={styles.button}
+      onPress={() => setCount(count + 1)}
+      onLongPress={() => setCount(0)}
+      >
+        <Text style={styles.buttonText}>+1</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texHuge: {
+    fontSize: 120,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  button: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#65558f',
+    padding: 20,
+    borderRadius: 15,
+    marginTop:20,
+    elevation:3,
+    shadowRadius:4,
+  }
+
 });
